5d3750bf19aaf23667d2d419e30617834cc953c7
lewisbrotchie <noble.ninja@gmail.com>
2018-04-23 17:03:21 +0100

    more packages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from "react";
+import styled, { injectGlobal } from "styled-components";
+
+import "typeface-roboto";
+
+const HeaderBackground = styled.img`
+  margin: 0;
+  height: 55%;
+  width: 95%;
+`;
 
 class App extends Component {
   render() {
-    return <div>hi</div>;
+    return (
+      <div>
+        <HeaderBackground src="https://images.pexels.com/photos/8724/pexels-photo-8724.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" />
+      </div>
+    );
   }
 }
 
