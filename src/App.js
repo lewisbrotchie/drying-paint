aca98d68143b6d3e5624a3f53ba9d2c6e616a033
lewisbrotchie <noble.ninja@gmail.com>
2018-04-24 14:46:51 +0100

    header image placement

--- src/App.js
+++ src/App.js
@@ -9,17 +9,23 @@ body {
 }
 `;
 
+const HeaderBackgroundWrapper = styled.div`
+  width: 100%;
+  height: 400px;
+  overflow: hidden;
+`;
+
 const HeaderBackground = styled.img`
-  margin: 0;
-  height: 55%;
-  width: 95%;
+  margin: -100px 0 0 -100px;
 `;
 
 class App extends Component {
   render() {
     return (
       <div>
-        <HeaderBackground src="https://images.pexels.com/photos/8724/pexels-photo-8724.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" />
+        <HeaderBackgroundWrapper>
+          <HeaderBackground src="https://images.pexels.com/photos/8724/pexels-photo-8724.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" />
+        </HeaderBackgroundWrapper>
       </div>
     );
   }
